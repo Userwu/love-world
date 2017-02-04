1a16deee543ff0d0d0c228dc79273e5fa49f2e77
wuhongxu <1102612827@qq.com>
2017-02-04 16:34:08 +0800

    修正页面

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, browserHistory ,IndexRoute} from 'react-router';
 import App from '../containers/App';
 import React from 'react';
 
 export default (
   <Router history={browserHistory}>
-    <Route path="/" component={App} />
+    <Route path="*" component={App} />
   </Router>
 )
